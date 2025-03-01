f9646960d41abb2c321897caa25af58a876fbbcb
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-01 21:00:56 +0700

    Re-add Variables as a regular folder

---
+++ Variables/Variable-Types/variables.js
@@ -0,0 +1,42 @@
+
+const myInteger = 12; // 32-bit number (from -2,147,483,648 to 2,147,483,647)
+const myLong = 9310141419482; // 64-bit number (from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807)
+const myFloat = 5.5; // 32-bit floating-point number (decimal)
+const myDouble = 9310141419482.22; // 64-bit floating-point number
+const myBoolean = true; // 1-bit true/false (0 or 1)
+const myBoolean2 = false; // 1-bit true/false (0 or 1)
+const myNotANumber = NaN; // Not a Number, special value
+const NaN_Example = 0 / 0; // NaN: Division by Zero is not possible
+const myInfinity = Infinity; // Represents infinity value
+const myBigNumber = 123456789012345678901234567890n; // BigInt number for arbitrary precision
+const mySymbol = Symbol('My Symbol'); // Unique and immutable data type
+const myNull = null; // Null value, represents "nothing"
+const myUndefined = undefined; // Undefined value, variable not assigned
+const myBigInt = BigInt(123456789012345678901234567890); // BigInt number for large integers
+const myObject = { // Object to store multiple related data
+    name: 'John', // String property
+    age: 30, // Number property
+    isMarried: true, // Boolean property
+    hobbies: ['Sports', 'Music', 'Reading'] // Array property
+};
+const myArray = [1, 2, 3, 4, 5]; // Array of numbers
+console.log("Different data types in JavaScript");
+console.log('An example of integer in JavaScript is ' +myInteger);
+console.log('An example of long in JavaScript is ' +myLong);
+console.log('An example of float in JavaScript is ' +myFloat);
+console.log('An example of double in JavaScript is ' +myDouble);
+console.log('An example of boolean in JavaScript is ' +myBoolean);
+console.log('An example of boolean2 in JavaScript is ' +myBoolean2);
+console.log('An example of Not a Number in JavaScript is ' +myNotANumber);
+console.log('An example of NaN in JavaScript is ' +NaN_Example);
+console.log('An example of Infinity in JavaScript is ' +myInfinity);
+console.log('An example of Big Number in JavaScript is ' +myBigNumber);
+console.log('An example of Symbol in JavaScript is ' +mySymbol);
+console.log('An example of Null in JavaScript is ' +myNull);
+console.log('An example of Undefined in JavaScript is ' +myUndefined);
+console.log('An example of BigInt in JavaScript is ' +myBigInt);
+console.log('An example of Object in JavaScript is ' +myObject);
+console.log('An example of Array in JavaScript is ' +myArray);
+
+
+
