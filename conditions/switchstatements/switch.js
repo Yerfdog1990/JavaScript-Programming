5a7166addcaf89f4d6457914975e160000d8fa9a
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-18 14:36:08 +0700

    Switch statements

--- conditions/switchstatements/switch.js
+++ conditions/switchstatements/switch.js
@@ -13,3 +13,20 @@ switch(score) {
         default:
             console.log("Score: F");
 }
+// The break statement "breaks" out of the switch statement and ensures no more code within the switch statement
+// is executed. This is how sections are defined and allows the user to make "fall through" cases.
+// Warning: lack of a break or return statement for each case means the program will continue to evaluate
+// the next case, even if the case criteria is unmet!
+// See example below.
+
+const score2 = 80;
+switch(score2) {
+    case 90:
+        console.log("Score: A");
+        case 80:
+            console.log("Score: B");
+            case 70:
+                console.log("Score: C");
+                default:
+                    console.log("Score: F");
+}
