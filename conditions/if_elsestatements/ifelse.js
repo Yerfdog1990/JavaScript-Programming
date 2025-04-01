7b1891d9fb36af5ec0afc6b54867dda9c561d6a6
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-04-01 13:58:14 +0700

    Eif-else control statements.

---
+++ conditions/if_elsestatements/ifelse.js
@@ -0,0 +1,10 @@
+// Execute a single statement
+const age = 10;
+if(age > 18) console.log("You are eligible to vote!");
+
+// Execute multiple statements
+if (age > 18) {
+    console.log("You are eligible to vote!");
+}else{
+    console.log("You are not eligible to vote!");
+}
